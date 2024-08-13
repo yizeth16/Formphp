0494a9989db893a41b694d059951214efa105c27
yizeth16 <yizethotavo@gmail.com>
2024-08-13 14:19:09 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,10 +1,14 @@
 <?php
-$Identificacion=$_POST ['Identificacion'];
-$NomApellidos=$_POST ['NomApellidos'];
-$Cargo=$_POST ['Cargo'];
-$Salario=$_POST ['Salario'];
-echo "La identficacion del empleado es: ".Identificacion;
-echo "Los nombres y  apellidos del empleado son: ".NomApellidos;
-echo "El cargo del empleado es: ".Cargo;
-echo "El salario del empleado es: ".Salario;
-?>
+       
+ $Identificacion= $_POST ['Identificacion'];
+$NomApellidos= $_POST ['NomApellidos'];
+$Cargo= $_POST ['Cargo'];
+$Salario= $_POST ['Salario'];
+echo "La identificacion del empleado es: ".$Identificacion;
+echo '</br>';
+echo "Los Nombres y Apellidos del empleado son: ".$NomApellidos;
+echo '</br>';
+echo "El cargo del empleado es: ".$Cargo;
+echo '</br>';
+echo "El salario del empleado es: ".$Salario;
+        ?>
